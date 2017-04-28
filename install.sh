ef794c45317538eaefad3b03f560df010840dc79
sh1nduu <sh1nduu@users.noreply.github.com>
2017-04-28 22:35:16 +0900

    Update install.sh and .bashrd

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 for dotfile in $(ls dotfiles);
 do
-  cp dotfiles/$dotfile ~/.$dotfile
-  echo 'set ~/.'${dotfile}
+  if [ -L $HOME/.$dotfile ]
+  then
+    rm $HOME/.$dotfile
+  else
+    mv $HOME/.$dotfile backups/$dotfile
+  fi
+  ln -s $PWD/dotfiles/$dotfile $HOME/.$dotfile
+  echo 'set '${HOME}'/.'${dotfile}
 done
