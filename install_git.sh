070f0b8f12399bd00c5b5a354d7abc6c8b60c4a8
sh1nduu <>
2017-08-04 03:06:09 +0900

    Add latest git install script

---
+++ install_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+yum install -y perl-devel openssl-devel curl-devel expat-devel gettext-devel zlib-devel
+
+GIT=git-2.13.4
+wget https://www.kernel.org/pub/software/scm/git/${GIT}.tar.gz
+tar zxvf ${GIT}.tar.gz
+
+make -C ${GIT} & make install -C ${GIT}
+cp ${GIT}/git /usr/bin/git
